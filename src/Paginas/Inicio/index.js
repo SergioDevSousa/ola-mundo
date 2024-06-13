381663eb1dd5bb9d5b067ff75d2bd4366070a64a
SergioDevSousa <166451865+SergioDevSousa@users.noreply.github.com>
2024-06-13 11:48:59 -0300

    aula 4 update

--- src/Paginas/Inicio/index.js
+++ src/Paginas/Inicio/index.js
@@ -1,14 +1,14 @@
 import styles from './Inicio.module.css';
 
 import posts from 'json/post.json';
-import Post from "Componentes/Post";
+import PostCard from "Componentes/PostCard";
 
 export default function Inicio() {
     return (
             <ul className={styles.post}>
                 {posts.map((post)=> (
                     <li key={post.id}>
-                        <Post post={post} />
+                        <PostCard post={post} />
                     </li>
                 ))}
 
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
+import PaginaPadrao from "Componentes/PaginaPadrao";
+import Rodape from "Componentes/Rodape";
+import NaoEncontrada from "./Componentes/PaginaNaoEncontrada";
+import Post from "Paginas/Post";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Menu from "./Componentes/Menu";
 import Inicio from './Paginas/Inicio';
 import SobreMim from './Paginas/SobreMim';
-import Rodape from "Componentes/Rodape";
-import PaginaPadrao from "Componentes/PaginaPadrao";
 
 function AppRoutes() {
   return (
@@ -14,9 +16,10 @@ function AppRoutes() {
         <Route path="/" element={<PaginaPadrao />}>
           <Route index element={<Inicio />} />
           <Route path="sobremim" element={<SobreMim />} />
+          <Route path="posts/:id" element={<Post />} />
         </Route>
-
-        <Route path="*" element={<div>Página não encontrada</div>} />
+        
+        <Route path="*" element={<NaoEncontrada />} />
       </Routes>
 
       <Rodape />
